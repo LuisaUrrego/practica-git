dac541cdbe43923fd02a91dd2d872d1980b85332
luisa urrego <luisafernandaurrego89@gmail.com>
2023-04-03 15:35:25 -0500

    clonacion del pokedex

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-
-const getPokemon = async (url) => {
-    const { data } = await axios.get(url);
-    console.log("api pokemons", data)
-    return data;
-    }
-getPokemon ("https://pokeapi.co/api/v2/pokemon/ditto")
