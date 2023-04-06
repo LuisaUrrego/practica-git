a0e00cf3394b839502dc63fedb9045090f5855eb
luisa urrego <luisafernandaurrego89@gmail.com>
2023-04-05 22:59:30 -0500

    obteniendo y pintando la informacion de la API

--- script/script.js
+++ script/script.js
@@ -30,7 +30,10 @@ const getAllInfoPokedex = async(url) => {
                 name: response.data.name,
                 abilities: response.data.abilities.map(item=> item.ability.name),
                 image: response.data.sprites.front_default,
-                height: response.data.height
+                height: response.data.height,
+                weight: response.data.weight,
+                types: response.data.types.type,
+                version_group_details:response.data.level_learned_at
             };
             allInfoPokedex.push(pokemon);   
         }
@@ -44,9 +47,27 @@ const getAllInfoPokedex = async(url) => {
 
 document.addEventListener('DOMContentLoaded', async () => {
     pokedex = await getPokeApi(URL_POKE);
-    console.log(pokedex);
+    console.log(pokedex)
 
-    const allinfo = await getAllInfoPokedex(URL_POKE);
-    console.log(allinfo);
+
+    const allInfo = await getAllInfoPokedex(URL_POKE);
+    const numeroPokemon = document.getElementById("numero_pokemon");
+    const weightPokemon = document.getElementById("weight_pokemon");
+    const heightPokemon = document.getElementById("height_pokemon");
+    const levelPokemon = document.getElementById("nivel_pokemon");
+    const typePokemon = document.getElementById("tipo_pokemon");
+    const habilidad_pokemon = document.getElementById("habilidad_pokemon");
+    const imagen_pokemon = document.getElementById("pokeImagenes");
+    const printPokemon = (tagIdPokemon, infoPokemon, tagHeightPokemon, tagWeightPokemon,tagTypePokemon, tagLevelPokemon, tagAbilitiesPokemon, tagImagesPokemon ) =>{
+    tagIdPokemon.innerHTML= `${infoPokemon[0].id} `
+    tagHeightPokemon.innerHTML= `${infoPokemon[0].height} m`
+    tagWeightPokemon.innerHTML= `${infoPokemon[0].weight} `
+    tagLevelPokemon.innerHTML= `${infoPokemon[0].version_group_details} `
+    tagTypePokemon.innerHTML= `${infoPokemon[0].types}`
+    tagAbilitiesPokemon.innerHTML= `${infoPokemon[0].abilities} `
+    tagImagesPokemon.innerHTML= `${infoPokemon[0].image} `
+
+    }
+    printPokemon(numeroPokemon, allInfo, heightPokemon, weightPokemon, typePokemon, levelPokemon, habilidad_pokemon, imagen_pokemon);
 })
 
