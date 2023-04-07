659ec5ce3fe670e75802a02285e193b1d01f93e6
Yolanda Vasquez <yolandavasquezhuer@hotmail.com>
2023-04-07 10:38:38 -0500

    Resolviendo conflictos de la integración

--- script/script.js
+++ script/script.js
@@ -60,30 +60,18 @@ document.addEventListener('DOMContentLoaded', async () => {
     const levelPokemon = document.getElementById("nivel_pokemon");
     const typePokemon = document.getElementById("tipo_pokemon");
     const habilidad_pokemon = document.getElementById("habilidad_pokemon");
-<<<<<<< HEAD
-    //const imagen_pokemon = document.getElementById("pokeImagenes");
-    const printPokemon = (tagIdPokemon, infoPokemon, tagHeightPokemon, tagWeightPokemon,tagTypePokemon, tagLevelPokemon, tagAbilitiesPokemon, /*tagImagesPokemon*/ ) =>{
-=======
     const imagen_pokemon = document.getElementById("pokeImagenes");
     const printPokemon = (tagIdPokemon,tagNombrePokemon, infoPokemon, tagHeightPokemon, tagWeightPokemon,tagTypePokemon, tagLevelPokemon, tagAbilitiesPokemon, tagImagesPokemon ) =>{
->>>>>>> feacture/lu
     tagIdPokemon.innerHTML= `${infoPokemon[0].id} `
     tagHeightPokemon.innerHTML= `${infoPokemon[0].height} m`
     tagWeightPokemon.innerHTML= `${infoPokemon[0].weight} `
     tagLevelPokemon.innerHTML= `${infoPokemon[0].version_group_details} `
     tagTypePokemon.innerHTML= `${infoPokemon[0].types}`
     tagAbilitiesPokemon.innerHTML= `${infoPokemon[0].abilities} `
-<<<<<<< HEAD
-    //tagImagesPokemon.innerHTML= `${infoPokemon[0].image} `
-
-    }
-    printPokemon(numeroPokemon, allInfo, heightPokemon, weightPokemon, typePokemon, levelPokemon, habilidad_pokemon, /*imagen_pokemon*/);
-=======
     tagImagesPokemon.src= `${infoPokemon[0].image} `
     tagNombrePokemon.innerHTML= `${infoPokemon[0].name} `
 
     }
     printPokemon(numeroPokemon, nombrePokemon, allInfo, heightPokemon, weightPokemon, typePokemon, levelPokemon, habilidad_pokemon, imagen_pokemon);
->>>>>>> feacture/lu
 })
 
