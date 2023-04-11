3bb71bd8d6de4f94e2b930c0ab7a2a9c233049a9
luisa urrego <luisafernandaurrego89@gmail.com>
2023-04-11 10:17:47 -0500

    funcionalidad input busqueda

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 
-let pokedex = [];
-let allInfoPokedex = [];
+// let pokedex = [];
+// let allInfoPokedex = [];
 const URL_POKE = "https://pokeapi.co/api/v2/pokemon?limit=5&offset=0"
 
 
@@ -21,35 +21,35 @@ const getPokeApi = async (url) => {
 
 
 
-const getAllInfoPokedex = async(url) => {
-    const allInfoPokedex = []
-    try {
-        const { data } = await axios.get(url); 
-        //console.log(data.results)
-        for (const pokedex of data.results) {
-            const urlPokedex = pokedex.url;
-            const response = await axios.get(urlPokedex);
-            //console.log(response)
-            const pokemon = {
-                id: response.data.id,
-                name: response.data.name,
-                abilities: response.data.abilities[0].ability.name,
-                image: response.data.sprites.front_default,
-                height: response.data.height,
-                weight: response.data.weight,
-                types: response.data.types.type,
-                version_group_details:response.data.level_learned_at
-            };
-            allInfoPokedex.push(pokemon);  
+// // const getAllInfoPokedex = async(url) => {
+//     const allInfoPokedex = []
+//     try {
+//         const { data } = await axios.get(url); 
+//         //console.log(data.results)
+//         for (const pokedex of data.results) {
+//             const urlPokedex = pokedex.url;
+//             const response = await axios.get(urlPokedex);
+//             //console.log(response)
+//             const pokemon = {
+//                 id: response.data.id,
+//                 name: response.data.name,
+//                 abilities: response.data.abilities[0].ability.name,
+//                 image: response.data.sprites.front_default,
+//                 height: response.data.height,
+//                 weight: response.data.weight,
+//                 types: response.data.types.type,
+//                 version_group_details:response.data.level_learned_at
+//             };
+//             allInfoPokedex.push(pokemon);  
             
-        }
-        return allInfoPokedex;
-    } catch (error) {
-        console.log(error);
-        return []
+//         }
+//         return allInfoPokedex;
+//     } catch (error) {
+//         console.log(error);
+//         return []
         
-    }
-}
+//     }
+// }
 
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -118,45 +118,59 @@ opciones.forEach((pokemon) => {
     });
   });
 });
+form.addEventListener ('submit', (e) => {
+    e.preventDefault ();
+    const busqueda = inputBusqueda.value
+    if (busqueda && busqueda !== '') {
+        getPokeApi (URL_POKE + busqueda)
+        busqueda.value = ''
+        
+    }
+    else {
+        window.location.reload()
+
+    }
+    console.log(busqueda)
+}) 
 
 
- const filtroBusqueda = (allInfoPokedex, apiUrl) => {
-       const pokemonsFiltrados = apiUrl.filter(pokemones=> pokemones.name.toLowerCase().includes(allInfoPokedex.toLowerCase()));
-       const result = pokemonsFiltrados.length ?
-       pokemonsFiltrados : apiUrl;
-       const messageResult = pokemonsFiltrados.length? true: "Este poekemon no exixte";
+//  const filtroBusqueda = (allInfo) => {
+//        const pokemonsFiltrados = apiUrl.filter(pokemones=> pokemones.name.toLowerCase().includes(allInfoPokedex.toLowerCase()));
+//        const result = pokemonsFiltrados.length ?
+//        pokemonsFiltrados : apiUrl;
+//        const messageResult = pokemonsFiltrados.length? true: "Este poekemon no exixte";
+      
+//        return {
+//         resultSearch: result,
+//         messageSearch: messageResult,
 
-       return {
-        resultSearch: result,
-        messageSearch: messageResult,
-
-       };
+//        };
 
     
- }
+//  }
 
- const busquedaPokemon = document.querySelector(".busqueda_pokemon");
- console.log (busquedaPokemon);
+//  const busquedaPokemon = document.querySelector(".busqueda_pokemon");
+//  console.log (busquedaPokemon);
 
 
- busquedaPokemon.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log('Buscar');
+//  busquedaPokemon.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     console.log('Buscar');
 
-    const terminoDeBusqueda = 'Buscar';
+//     const terminoDeBusqueda = 'Buscar';
 
-    if (terminoDeBusqueda) {
-        const resultadoDeBusqueda = filtroBusqueda(terminoDeBusqueda, URL_POKE);
-        console.log (resultadoDeBusqueda);
+//     if (terminoDeBusqueda) {
+//         const resultadoDeBusqueda = filtroBusqueda(terminoDeBusqueda, URL_POKE);
+//         console.log (resultadoDeBusqueda);
 
-        getPokeApi(allInfoPokedex,resultadoDeBusqueda,URL_POKE);
+//         getPokeApi(allInfo,resultadoDeBusqueda,URL_POKE);
         
-        if(resultadoDeBusqueda.messageSearch) {
-            Swal.fire("Oops!", resultadoDeBusqueda,messageSearch, "error");
-        }
+//         if(resultadoDeBusqueda.messageSearch) {
+//             Swal.fire("Oops!", resultadoDeBusqueda,messageSearch, "error");
+//         }
 
-    }else {
-        Swal.fire("Oops!", "No has ingrasado un término de búsqueda", "error");
-    }
+//     }else {
+//         Swal.fire("Oops!", "No has ingrasado un término de búsqueda", "error");
+//     }
 
- })
+//  })
